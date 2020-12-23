b3c63711a2927fb5648fd601d4d974269b7aebb7
Haseeb Abid <haseeb.abid5@gmail.com>
2020-12-23 18:34:29 -0500

    Level and Player Setup
    Set up level layour with tilemap. Created player with modular body sprites and basic character controller. Also added collision between level elements and player.

---
+++ ShopChallenge/Assets/PlayerActions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerActions : MonoBehaviour
+{
+    public float moveSpeed = 2f;
+    public Rigidbody2D rb2d;
+
+    Vector2 moveVector;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        moveVector.x = Input.GetAxisRaw("Horizontal");
+        moveVector.y = Input.GetAxisRaw("Vertical");
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb2d.MovePosition(rb2d.position + moveVector * moveSpeed * Time.fixedDeltaTime);
+    }
+}
